f6b78aa0a463874db13f1333385d30900b6ea692
DavidLichenko <89989857+DavidLichenko@users.noreply.github.com>
2024-12-29 11:24:40 +0000

    Update main.py

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -109,4 +109,4 @@ async def get_real_time_candle(symbol: str):
 
 # Run FastAPI app
 if __name__ == "__main__":
-    uvicorn.run(app, host="https://aragon-trade.com/", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
