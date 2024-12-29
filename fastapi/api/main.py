bab1750bb6c07d8366e3ee6288ca2a7ad73ea63d
DavidLichenko <89989857+DavidLichenko@users.noreply.github.com>
2024-12-29 13:27:38 +0000

    Update main.py

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -5,17 +5,51 @@ from concurrent.futures import ThreadPoolExecutor
 
 app = FastAPI()
 
-@app.get("/api/stocks/{symbol}/candlesticks")
-def get_historical_candlesticks():
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['https://localhost:3000','https://127.0.0.1:58540','http://localhost:3000','http://80.137.37.62','https:80.137.37.62','http://80.137.37.62:59402','https:80.137.37.62:59402','https://aragon-trade.com'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get("/api/stocks/{symbol}/candlesticks/{")
+def get_historical_candlesticks(symbol: str, timeframe: str = "1 min", duration: str = "1 D"):
     # Create a new event loop for the thread
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)  # Set the new event loop as the current loop
 
     ib = IB()
     loop.run_until_complete(ib.connectAsync('127.0.0.1', 7497, clientId=1))
-    
+    contract = Stock(symbol, 'SMART', 'USD')
+    bars = ib.reqHistoricalData(
+        contract,
+        endDateTime='',
+        durationStr=duration,
+        barSizeSetting=timeframe,
+        whatToShow='TRADES',
+        useRTH=True
+    )
+
+    if not bars:
+        raise HTTPException(status_code=404, detail="No historical data found for the given symbol.")
+
+        # Convert data to JSON format for frontend
+    data = [
+        {
+            "time": bar.date.isoformat(),
+            "open": bar.open,
+            "high": bar.high,
+            "low": bar.low,
+            "close": bar.close,
+            "volume": bar.volume
+        }
+        for bar in bars
+    ]
+    return data
     # Perform your tasks (e.g., requesting historical data)
     ib.disconnect()
+
 
 
 # # Real-Time Candle Updates Endpoint
